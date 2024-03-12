19f67d16a3f630c6c624e2d458931ce9a7ed3972
Eric Fileman <emfileman@gmail.com>
2024-03-12 18:46:32 +0000

    changed back to docker

--- tests/deploystack.sh
+++ tests/deploystack.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 #
-# Deploy podman stack
+# Deploy docker stack
 #
 # Compose file
-DOCKER_COMPOSE=${DOCKER_COMPOSE:=podman-compose.yaml}
+DOCKER_COMPOSE=${DOCKER_COMPOSE:=docker-compose.yaml}
 
 # Images
 SNAKEMAKE_IMAGE=${SNAKEMAKE_IMAGE:=quay.io/biocontainers/snakemake:7.32.4--hdfd78af_1}
 HTCONDORIMAGE=${HTCONDORIMAGE:=docker.io/htcondor/mini:23.0-el8}
 
-podman pull $SNAKEMAKE_IMAGE
-podman pull $HTCONDORIMAGE
+docker pull $SNAKEMAKE_IMAGE
+docker pull $HTCONDORIMAGE
 
 # Stack and service config
 STACK_NAME=cookiecutter-htcondor
@@ -28,13 +28,13 @@ function service_up {
     COUNT=1
     MAXCOUNT=30
 
-    podman service ps $SERVICE --format "{{.CurrentState}}" 2>/dev/null | grep Running
+    docker service ps $SERVICE --format "{{.CurrentState}}" 2>/dev/null | grep Running
     service_up=$?
 
     until [ $service_up -eq 0 ]; do
 	echo "$COUNT: service $SERVICE unavailable"
 	sleep 5
-	podman service ps $SERVICE --format "{{.CurrentState}}" 2>/dev/null | grep Running
+	docker service ps $SERVICE --format "{{.CurrentState}}" 2>/dev/null | grep Running
 	service_up=$?
 	if [ $COUNT -eq $MAXCOUNT ]; then
 	    echo "service $SERVICE not found; giving up"
@@ -51,25 +51,25 @@ function service_up {
 ## Deploy stack
 ##############################
 
-# Check if podman stack has been deployed
-podman service ps $HTCONDOR_SERVICE --format "{{.CurrentState}}" 2>/dev/null | grep Running
+# Check if docker stack has been deployed
+docker service ps $HTCONDOR_SERVICE --format "{{.CurrentState}}" 2>/dev/null | grep Running
 service_up=$?
 
 if [ $service_up -eq 1 ]; then
-    podman play kube $DOCKER_COMPOSE;
+    docker play kube $DOCKER_COMPOSE;
 fi
 
 service_up $HTCONDOR_SERVICE
 service_up $SNAKEMAKE_SERVICE
-CONTAINER=$(podman ps | grep cookiecutter-htcondor_htcondor | awk '{print $1}')
+CONTAINER=$(docker ps | grep cookiecutter-htcondor_htcondor | awk '{print $1}')
 
 # Fix snakemake header to point to /opt/local/bin
-podman exec $CONTAINER /bin/bash -c "head -1 /opt/local/bin/snakemake" | grep -q "/usr/local/bin"
+docker exec $CONTAINER /bin/bash -c "head -1 /opt/local/bin/snakemake" | grep -q "/usr/local/bin"
 if [ $? -eq 0 ]; then
     echo "Rewriting snakemake header to point to /opt/local/bin"
-    podman exec $CONTAINER /bin/bash -c 'sed -i -e "s:/usr:/opt:" /opt/local/bin/snakemake'
+    docker exec $CONTAINER /bin/bash -c 'sed -i -e "s:/usr:/opt:" /opt/local/bin/snakemake'
 fi
 
 # Add htcondor to snakemake
-CONTAINER=$(podman ps | grep cookiecutter-htcondor_snakemake | awk '{print $1}')
-podman exec $CONTAINER 'pip install htcondor && ldd --version' 
+CONTAINER=$(docker ps | grep cookiecutter-htcondor_snakemake | awk '{print $1}')
+docker exec $CONTAINER 'pip install htcondor && ldd --version'
